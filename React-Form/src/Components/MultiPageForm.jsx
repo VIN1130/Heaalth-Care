e29e745e70719d8110eed876ddb6ca4285ec986d
VIN1130 <161826400+VIN1130@users.noreply.github.com>
2024-07-06 15:08:11 +0200

    feat: 3rd commit

--- React-Form/src/Components/MultiPageForm.jsx
+++ React-Form/src/Components/MultiPageForm.jsx
@@ -5,62 +5,73 @@ import FamilyRelationsForm from './FamilyRelationsForm';
 import { formReducer, initialState } from './formReducer';
 
 const MultiPageForm = () => {
-  const [state, dispatch] = useReducer(formReducer, initialState);
+    const [state, dispatch] = useReducer(formReducer, initialState);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    dispatch({ type: 'UPDATE_FORM_DATA', payload: { name, value } });
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        dispatch({ type: 'UPDATE_FORM_DATA', payload: { name, value } });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch({ type: 'NEXT_PAGE' });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({ type: 'NEXT_PAGE' });
+    };
 
-  const handleBack = () => {
-    dispatch({ type: 'PREVIOUS_PAGE' });
-  };
+    const handleBack = () => {
+        dispatch({ type: 'PREVIOUS_PAGE' });
+    };
 
-  const addFamilyMember = () => {
-    dispatch({ type: 'ADD_FAMILY_MEMBER' });
-  };
+    const addFamilyMember = () => {
+        dispatch({ type: 'ADD_FAMILY_MEMBER' });
+    };
 
-  const updateFamilyMember = (e, index) => {
-    const { name, value } = e.target;
-    dispatch({ type: 'UPDATE_FAMILY_MEMBER', payload: { name, value, index } });
-  };
+    const updateFamilyMember = (e, index) => {
+        const { name, value } = e.target;
+        dispatch({ type: 'UPDATE_FAMILY_MEMBER', payload: { name, value, index } });
+    };
 
-  const { page, formData } = state;
+    const { page, formData } = state;
 
-  return (
-    <div>
-      {page === 1 && (
-        <BasicInfoForm
-          formData={formData}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-        />
-      )}
-      {page === 2 && (
-        <MedicalHistoryForm
-          formData={formData}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-          handleBack={handleBack}
-        />
-      )}
-      {page === 3 && (
-        <FamilyRelationsForm
-          formData={formData}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-          handleBack={handleBack}
-          addFamilyMember={addFamilyMember}
-          updateFamilyMember={updateFamilyMember}
-        />
-      )}
-    </div>
-  );
+
+    const renderPage = () => {
+        switch (page) {
+            case 1:
+                return <BasicInfoForm
+                    formData={formData}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                />
+            case 2:
+                return <MedicalHistoryForm
+                    formData={formData}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                    handleBack={handleBack}
+                />
+
+            case 3:
+                return <FamilyRelationsForm
+                    formData={formData}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                    handleBack={handleBack}
+                    addFamilyMember={addFamilyMember}
+                    updateFamilyMember={updateFamilyMember}
+                />
+
+            default:
+                return <p>Page does not exist</p>
+
+        }
+    }
+
+
+    return (
+        <div>
+            {renderPage()}
+
+        </div>
+    );
 };
 
 export default MultiPageForm;
